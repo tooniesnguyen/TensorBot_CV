b9b678e5127f91bdc67b654c2233ce7c123dd52b
sudo ./ubuntu-initial.sh <nnnhan1882002@gmail.com>
2023-11-09 11:23:59 +0700

    update

--- client.py
+++ client.py
@@ -1,29 +1,26 @@
-import socket,cv2, pickle,struct
+import uvicorn
+from fastapi import FastAPI, WebSocket
+import cv2
+import numpy as np
 
-# create socket
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '192.168.71.11' # paste your server ip address here
-port = 9999
-client_socket.connect((host_ip,port)) # a tuple
-data = b""
-payload_size = struct.calcsize("Q")
-while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) # 4K
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
-	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key  == ord('q'):
-		break
-client_socket.close()
-	
+
+
+app = FastAPI()
+
+
+
+
+
+@app.websocket("/video_feed")
+async def video_feed(websocket: WebSocket):
+    await websocket.accept()
+    vid = cv2.VideoCapture(0)  # Khởi tạo camera ở đây
+    while True:
+        ret, frame = vid.read()
+        await websocket.send_bytes(frame)
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 8000)
